a086effd288f679b17792331ee8a7f3944c5d9f9
Tyler Ellingford <tyler@Rickys-MacBook-Pro.local>
2019-05-17 10:37:29 -0600

    completed part 1 step 1

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -3,11 +3,43 @@ import React, {Component} from 'react';
 class Form extends Component {
     constructor(){
         super();
+
+        this.state = {
+            imageURLInput: '',
+            productNameInput: '',
+            priceInput: null,
+        }
+    }
+
+    handleInputChange = (e) => {
+        e.preventDefault();
+        this.setState({
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    handleCancelInput = (e) => {
+        e.preventDefault();
+        this.setState({
+            imageURLInput: '',
+            productNameInput: '',
+            priceInput: null,
+        })
     }
 
     render(){
         return(
-            <div>Form</div>
+            <div>
+                <form>
+                    <input name='imageURLInput' value={this.state.imageURLInput} onChange={this.handleInputChange} />
+                    <input name='productNameInput' value={this.state.productNameInput} onChange={this.handleInputChange} />
+                    <input name='priceInput' value={this.state.priceInput} onChange={this.handleInputChange} />
+                    <div>
+                        <button onClick={this.handleCancelInput}>Cancel</button>
+                        <button>Add to Inventory</button>
+                    </div>
+                </form>
+            </div>
         )
     }
 }
